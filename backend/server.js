6e0946f2a5c38a5e7c884f07c6b320cfc759ce26
Abdus <asdevlpr12@gmail.com>
2020-12-13 20:25:12 -0800

    preDeployment Finished project

--- backend/server.js
+++ backend/server.js
@@ -21,12 +21,20 @@ process.env.NODE_ENV==='development' && app.use(morgan('dev'))
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 app.use("/api/uploads",uploadRoutes)
-app.get("/",(req,res)=>{ res.send("API is running...") })
 
 app.use("/api/products",productRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/orders",orderRoutes)
 app.use('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID))
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname,'/frontend/build')))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'frontend/build/index.html'))
+  })
+}else{
+  app.get("/",(req,res)=>{ res.send("API is running...") })
+
+}
 /* Custom Error Handler */
 app.use(notFound); app.use(errorHandler);
 
